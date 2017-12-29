f8523049685b244a7c2f3bbfe9b8347c5c851fe0
eal6gf <elum1234@gmail.com>
2017-12-28 22:43:25 -0500

    add secrets loader for keys

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def google_maps(latitude, longitude)
+    # "https://maps.googleapis.com/maps/api/staticmap?&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7C%7C#{latitude},#{longitude}key=AIzaSyDGvgS-p6cR5loLAz3OVlo6sutDlQ8Bz1A"
+
+  "https://maps.googleapis.com/maps/api/staticmap?&zoom=15&size=200x200&maptype=roadmap&markers=color:red%7C%7C#{latitude},#{longitude}&key=#{ENV['SUPER_SECRET_KEY']}"
+  end
 end
---
+++ config.js
@@ -0,0 +1,3 @@
+var config = {
+  SECRET_KEY: 'AIzaSyDGvgS-p6cR5loLAz3OVlo6sutDlQ8Bz1A'
+}
