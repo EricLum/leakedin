be4bfc5e566fa92906b6c9b78c28ba9714e46a1b
eal6gf <elum1234@gmail.com>
2017-12-31 21:52:12 -0500

    add model validations

--- app/models/bathroom.rb
+++ app/models/bathroom.rb
@@ -2,4 +2,8 @@ class Bathroom < ApplicationRecord
   has_many :ratings
   geocoded_by :address
   after_validation :geocode
+  validates :stalls, numericality: {only_integer: true, greater_than: 0, less_than: 50}
+  validates :title, length: {in: 1..50}
+  validates :establishment, presence: true
+  validates :address, presence: true
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,8 @@
 class Rating < ApplicationRecord
   belongs_to :bathroom
+  #validations
+  validates :title, presence: true, length: {in: 5..100}
+  validates :description, presence: true
+  validates :stars,  numericality: {only_integer: true, less_than: 6, greater_than: 0 }
+  validates :review_date, presence: true
 end
